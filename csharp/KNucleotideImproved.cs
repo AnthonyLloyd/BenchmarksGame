d53e570590ef34a5fe3299d703b090014b13d1c2
Anthony Lloyd <anthony@lloydhq.com>
2017-06-27 11:33:56 +0100

    knucleotide

--- csharp/KNucleotideImproved.cs
+++ csharp/KNucleotideImproved.cs
@@ -8,16 +8,63 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-public class KNucleotideImproved
+class KNucleotideLinkedPage { public byte[] data; public int length; public KNucleotideLinkedPage next; }
+
+public static class KNucleotideImproved
 {
+    const int READER_BUFFER_SIZE = 1024 * 128;
+    static BlockingCollection<page> readQue = new BlockingCollection<page>();
+    static ConcurrentBag<byte[]> bytePool = new ConcurrentBag<byte[]>(); 
+
+    static byte[] borrowBuffer()
+    {
+        byte[] ret;
+        return bytePool.TryTake(out ret) ? ret : new byte[READER_BUFFER_SIZE];
+    }
+
+    static void returnBuffer(byte[] bytes)
+    {
+        bytePool.Add(bytes);
+    }
+
+    static void Reader()
+    {
+        using (var stream = File.OpenRead(@"C:\temp\input25000000.txt"))//Console.OpenStandardInput())
+        {
+            for (;;)
+            {
+                var buffer = borrowBuffer();
+                var bytesRead = stream.Read(buffer, 0, READER_BUFFER_SIZE);
+                if (bytesRead == 0) break;
+                readQue.Add(new page { data = buffer, length = bytesRead });
+            }
+            readQue.CompleteAdding();
+        }
+    }
+
+    static bool tryTake<T>(BlockingCollection<T> q, out T t) where T : class
+    {
+        t = null;
+        while(!q.IsCompleted && !q.TryTake(out t)) Thread.SpinWait(0);
+        return t!=null;
+    }
+
+    static void Locator()
+    {
+        
+    }
+
     public static void Main(string[] args)
     {   
+        Task.Run((Action)Reader);
+        Task.Run((Action)Locator);
         PrepareLookups();
-        var buffer = GetBytesForThirdSequence();
         var task1 = Task.Factory.StartNew(() => CountFrequency(buffer, 1));
         var task2 = Task.Factory.StartNew(() => CountFrequency(buffer, 2));
         var task3 = Task.Factory.StartNew(() => CountFrequency(buffer, 3));
@@ -103,22 +150,6 @@ public class KNucleotideImproved
                 dictionary.Add(rollingKey, new Wrapper2 { v = 1 });
         }
         return dictionary;
-    }
-
-    private static byte[] GetBytesForThirdSequence()
-    {
-        var bytes = new List<byte>();
-        using (var r = File.OpenText("input25000000.txt"))//new StreamReader(Console.OpenStandardInput()))
-        {
-            while (!r.ReadLine().StartsWith(">THREE"));
-            var line = r.ReadLine();
-            while (line != null && line[0] != '>')
-            {
-                bytes.AddRange(Encoding.ASCII.GetBytes(line));
-                line = r.ReadLine();
-            }
-        }
-        return bytes.ToArray();
     }
 
     private static byte[] tonum = new byte[256];
