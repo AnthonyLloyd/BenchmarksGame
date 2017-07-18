84c45eef19c5d5da7c314b459b98820ac0a93029
Anthony Lloyd <anthony@lloydhq.com>
2017-07-18 22:43:45 +0100

    knuc winning

--- csharp/KNucleotideImproved.cs
+++ csharp/KNucleotideImproved.cs
@@ -12,32 +12,28 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-class WrapperImproved { public int v; }
+class WrapperImproved { public int v=1; }
 public static class KNucleotideImproved
 {
     static byte[] tonum = new byte[256];
     static char[] tochar = new char[] {'A', 'C', 'G', 'T'};
-
-    static string printKey(long key, int fragmentLength)
-    {
-        var items = new char[fragmentLength];
-        for (int i=fragmentLength-1; i>=0; --i)
-        {
-            items[i] = tochar[key & 0x3];
-            key >>= 2;
-        }
-        return new string(items);
-    }
 
     static StringBuilder writeFrequencies(Dictionary<long, WrapperImproved> freq, int buflen, int fragmentLength)
     {
         var sb = new StringBuilder();
         double percent = 100.0 / (buflen - fragmentLength + 1);
         foreach(var kv in freq.OrderByDescending(i => i.Value.v))
-        {       
-            sb.Append(printKey(kv.Key, fragmentLength));
+        {
+            var keyChars = new char[fragmentLength];
+            var key = kv.Key;
+            for (int i=keyChars.Length-1; i>=0; --i)
+            {
+                keyChars[i] = tochar[key & 0x3];
+                key >>= 2;
+            }
+            sb.Append(keyChars);   
             sb.Append(" ");
-            sb.AppendLine(((kv.Value.v+1) * percent).ToString("F3"));
+            sb.AppendLine((kv.Value.v * percent).ToString("F3"));
         }
         return sb;
     }
@@ -47,11 +43,10 @@ public static class KNucleotideImproved
         long key = 0;
         for (int i=0; i<fragment.Length; ++i)
         {
-            key <<= 2;
-            key |= tonum[fragment[i]];
+            key = (key << 2) | tonum[fragment[i]];
         }
         WrapperImproved w;
-        var n = dictionary.TryGetValue(key, out w) ? w.v+1 : 1;
+        var n = dictionary.TryGetValue(key, out w) ? w.v : 0;
         return n+"\t"+fragment;
     }
 
@@ -72,7 +67,7 @@ public static class KNucleotideImproved
         return d0;
     }
 
-    static Dictionary<long,WrapperImproved> calcDictionary(byte[] bytes, int l, int mask, int start, int end)
+    static Dictionary<long,WrapperImproved> calcDictionary(byte[] bytes, int l, long mask, int start, int end)
     {
         long rollingKey=0;
         while(--l>0)
@@ -92,9 +87,9 @@ public static class KNucleotideImproved
         return dict;
     }
 
-    static Task<Dictionary<long,WrapperImproved>>[] dictionaryTasks(byte[] bytes, int l, int mask, int n)
+    static Task<Dictionary<long,WrapperImproved>>[] dictionaryTasks(byte[] bytes, int bytesLength, int l, long mask, int n)
     {
-        int step = (bytes.Length-l)/n+1;
+        int step = (bytesLength-l)/n+1;
         var tasks = new Task<Dictionary<long,WrapperImproved>>[n];
         for(int i=0; i<n-1; i++)
         {
@@ -102,7 +97,7 @@ public static class KNucleotideImproved
             var end = start+l-1+step;
             tasks[i] = Task.Run(() => calcDictionary(bytes, l, mask, start, end));
         }
-        tasks[n-1] = Task.Run(() => calcDictionary(bytes, l, mask, (n-1)*step, bytes.Length));
+        tasks[n-1] = Task.Run(() => calcDictionary(bytes, l, mask, (n-1)*step, bytesLength));
         return tasks;
     }
 
@@ -112,48 +107,53 @@ public static class KNucleotideImproved
         tonum['g'] = 2; tonum['G'] = 2;
         tonum['t'] = 3; tonum['T'] = 3;
 
-        byte[] bytes;
+        var bytes = new byte[1048576];
+        int position = 0;
         using (var stream = new StreamReader(File.OpenRead(@"C:\temp\input25000000.txt")/*Console.OpenStandardInput()*/))
         {
             string line = stream.ReadLine();
             while(line[0]!='>' || line[1]!='T' || line[2]!='H' || line[3]!='R' || line[4]!='E' || line[5]!='E')
                 line = stream.ReadLine();
 
-            var bytesList = new List<byte>(1048576);
-
             while((line=stream.ReadLine()) != null && line[0]!='>')
             {
+                if (line.Length + position > bytes.Length)
+                {
+                    var newBytes = new byte[bytes.Length*2];
+                    Buffer.BlockCopy(bytes, 0, newBytes, 0, position);
+                    bytes = newBytes;
+                }
                 for(int i=0; i<line.Length; ++i)
                 {
-                    bytesList.Add(tonum[line[i]]);
+                    bytes[position++] = tonum[line[i]];
                 }
             }
-
-            bytes = bytesList.ToArray();
         }
 
         int nParallel = 4;//Environment.ProcessorCount
 
-        var taskDict18 = dictionaryTasks(bytes, 18, 16383, nParallel);
-        var taskDict12 = dictionaryTasks(bytes, 12, 4095, nParallel);
-        var taskDict6 = dictionaryTasks(bytes, 6, 1023, nParallel);
-        var taskDict4 = dictionaryTasks(bytes, 4, 255, nParallel);
-        var taskDict3 = dictionaryTasks(bytes, 3, 63, nParallel);
-        var taskDict2 = dictionaryTasks(bytes, 2, 15, 1);
-        var taskDict1 = dictionaryTasks(bytes, 1, 3, 1);
+        var taskDict18 = dictionaryTasks(bytes, position, 18, 68719476735, nParallel);
+        var taskDict12 = dictionaryTasks(bytes, position, 12, 16777215, nParallel);
+        var taskDict6 = dictionaryTasks(bytes, position, 6, 4095, nParallel);
+        var taskDict4 = dictionaryTasks(bytes, position, 4, 255, nParallel);
+        var taskDict3 = dictionaryTasks(bytes, position, 3, 63, nParallel);
+        var taskDict2 = dictionaryTasks(bytes, position, 2, 15, nParallel);
+        var taskDict1 = dictionaryTasks(bytes, position, 1, 3, nParallel);
         
         var taskString18 = Task.Factory.ContinueWhenAll(taskDict18, t => writeCount(merge(t), "GGTATTTTAATTTATAGT"));
         var taskString12 = Task.Factory.ContinueWhenAll(taskDict12, t => writeCount(merge(t), "GGTATTTTAATT"));
         var taskString6 = Task.Factory.ContinueWhenAll(taskDict6, t => writeCount(merge(t), "GGTATT"));
         var taskString4 = Task.Factory.ContinueWhenAll(taskDict4, t => writeCount(merge(t), "GGTA"));
         var taskString3 = Task.Factory.ContinueWhenAll(taskDict3, t => writeCount(merge(t), "GGT"));
-        var taskString1and2 = Task.Factory.ContinueWhenAll(new []{taskDict1[0], taskDict2[0]}, t =>
+        var taskString1and2 = Task.Factory.ContinueWhenAll(taskDict1, _ =>
         {
             int buflen = 0;
-            foreach(var w in t[0].Result.Values) buflen += w.v+1;
-            var sb = writeFrequencies(t[0].Result, buflen, 1);
+            var dict1 = merge(taskDict1);
+            foreach(var w in dict1.Values) buflen += w.v;
+            var sb = writeFrequencies(dict1, buflen, 1);
             sb.AppendLine();
-            sb.Append(writeFrequencies(t[1].Result, buflen, 2));
+            var dict2 = merge(taskDict2);
+            sb.Append(writeFrequencies(dict2, buflen, 2));
             return sb;
         });
 
