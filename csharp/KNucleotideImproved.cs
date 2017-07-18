39026804f1c1de5115f0e26ea85239e3e0189795
Anthony Lloyd <anthony@lloydhq.com>
2017-07-18 15:54:26 +0100

    knuc

--- csharp/KNucleotideImproved.cs
+++ csharp/KNucleotideImproved.cs
@@ -10,15 +10,11 @@ using System.IO;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 class WrapperImproved { public int v; }
 public static class KNucleotideImproved
 {
-    const int COUNT_LENGTH = 7;
-    readonly static int[] countKeyLengths = new int[COUNT_LENGTH] {18,12,6,4,3,2,1};
-
     static byte[] tonum = new byte[256];
     static char[] tochar = new char[] {'A', 'C', 'G', 'T'};
 
@@ -33,7 +29,7 @@ public static class KNucleotideImproved
         return new string(items);
     }
 
-    static void writeFrequencies(Dictionary<long, WrapperImproved> freq, int buflen, int fragmentLength)
+    static StringBuilder writeFrequencies(Dictionary<long, WrapperImproved> freq, int buflen, int fragmentLength)
     {
         var sb = new StringBuilder();
         double percent = 100.0 / (buflen - fragmentLength + 1);
@@ -43,10 +39,10 @@ public static class KNucleotideImproved
             sb.Append(" ");
             sb.AppendLine(((kv.Value.v+1) * percent).ToString("F3"));
         }
-        Console.Out.WriteLine(sb.ToString());
+        return sb;
     }
 
-    static void writeCount(Dictionary<long, WrapperImproved> dictionary, string fragment)
+    static string writeCount(Dictionary<long, WrapperImproved> dictionary, string fragment)
     {
         long key = 0;
         for (int i=0; i<fragment.Length; ++i)
@@ -56,7 +52,7 @@ public static class KNucleotideImproved
         }
         WrapperImproved w;
         var n = dictionary.TryGetValue(key, out w) ? w.v+1 : 1;
-        Console.Out.WriteLine(n+"\t"+fragment);
+        return n+"\t"+fragment;
     }
 
     static Dictionary<long,WrapperImproved> merge(Task<Dictionary<long,WrapperImproved>>[] dicts)
@@ -76,140 +72,96 @@ public static class KNucleotideImproved
         return d0;
     }
 
+    static Dictionary<long,WrapperImproved> calcDictionary(byte[] bytes, int l, int mask, int start, int end)
+    {
+        long rollingKey=0;
+        while(--l>0)
+        {
+            rollingKey = (rollingKey << 2) | bytes[start++];
+        }
+        var dict = new Dictionary<long,WrapperImproved>();
+        while(start<end)
+        {
+            rollingKey = ((rollingKey<<2) & mask) | bytes[start++];
+            WrapperImproved w;
+            if (dict.TryGetValue(rollingKey, out w))
+                w.v++;
+            else
+                dict.Add(rollingKey, new WrapperImproved());        
+        }
+        return dict;
+    }
+
+    static Task<Dictionary<long,WrapperImproved>>[] dictionaryTasks(byte[] bytes, int l, int mask, int n)
+    {
+        int step = (bytes.Length-l)/n+1;
+        var tasks = new Task<Dictionary<long,WrapperImproved>>[n];
+        for(int i=0; i<n-1; i++)
+        {
+            var start = i*step;
+            var end = start+l-1+step;
+            tasks[i] = Task.Run(() => calcDictionary(bytes, l, mask, start, end));
+        }
+        tasks[n-1] = Task.Run(() => calcDictionary(bytes, l, mask, (n-1)*step, bytes.Length));
+        return tasks;
+    }
+
     public static void Main(string[] args)
     {
         tonum['c'] = 1; tonum['C'] = 1;
         tonum['g'] = 2; tonum['G'] = 2;
         tonum['t'] = 3; tonum['T'] = 3;
 
+        byte[] bytes;
         using (var stream = new StreamReader(File.OpenRead(@"C:\temp\input25000000.txt")/*Console.OpenStandardInput()*/))
         {
-
             string line = stream.ReadLine();
             while(line[0]!='>' || line[1]!='T' || line[2]!='H' || line[3]!='R' || line[4]!='E' || line[5]!='E')
                 line = stream.ReadLine();
 
-            var bytes = new List<byte>(1048576);
+            var bytesList = new List<byte>(1048576);
 
             while((line=stream.ReadLine()) != null && line[0]!='>')
             {
                 for(int i=0; i<line.Length; ++i)
                 {
-                    bytes.Add(tonum[line[i]]);
+                    bytesList.Add(tonum[line[i]]);
                 }
             }
+
+            bytes = bytesList.ToArray();
         }
 
-        var dictionaryTasks = new Task<Dictionary<long,WrapperImproved>>[COUNT_LENGTH][];
-        for(int i=0; i<COUNT_LENGTH; i++)
+        int nParallel = 4;//Environment.ProcessorCount
+
+        var taskDict18 = dictionaryTasks(bytes, 18, 16383, nParallel);
+        var taskDict12 = dictionaryTasks(bytes, 12, 4095, nParallel);
+        var taskDict6 = dictionaryTasks(bytes, 6, 1023, nParallel);
+        var taskDict4 = dictionaryTasks(bytes, 4, 255, nParallel);
+        var taskDict3 = dictionaryTasks(bytes, 3, 63, nParallel);
+        var taskDict2 = dictionaryTasks(bytes, 2, 15, 1);
+        var taskDict1 = dictionaryTasks(bytes, 1, 3, 1);
+        
+        var taskString18 = Task.Factory.ContinueWhenAll(taskDict18, t => writeCount(merge(t), "GGTATTTTAATTTATAGT"));
+        var taskString12 = Task.Factory.ContinueWhenAll(taskDict12, t => writeCount(merge(t), "GGTATTTTAATT"));
+        var taskString6 = Task.Factory.ContinueWhenAll(taskDict6, t => writeCount(merge(t), "GGTATT"));
+        var taskString4 = Task.Factory.ContinueWhenAll(taskDict4, t => writeCount(merge(t), "GGTA"));
+        var taskString3 = Task.Factory.ContinueWhenAll(taskDict3, t => writeCount(merge(t), "GGT"));
+        var taskString1and2 = Task.Factory.ContinueWhenAll(new []{taskDict1[0], taskDict2[0]}, t =>
         {
-            int c = countKeyLengths[i];
-            int n = Environment.ProcessorCount;
-            
-        }   
-        
-        var task0 = Task.Factory.ContinueWhenAll(dictionaryTasks[0], merge);
-        
-        var taskz = task0.ContinueWith(dict => "");
-            
-        var dict6 = countDictionary[6];
-        int buflen = 0;
-        foreach(var w in dict6.Values) buflen += w.v+1;
-        writeFrequencies(dict6, buflen, countKeyLengths[6]);
-        writeFrequencies(countDictionary[5], buflen, countKeyLengths[5]);
-        writeCount(countDictionary[4], "GGT");
-        writeCount(countDictionary[3], "GGTA");
-        writeCount(countDictionary[2], "GGTATT");
-        writeCount(countDictionary[1], "GGTATTTTAATT");
-        writeCount(countDictionary[0], "GGTATTTTAATTTATAGT");
+            int buflen = 0;
+            foreach(var w in t[0].Result.Values) buflen += w.v+1;
+            var sb = writeFrequencies(t[0].Result, buflen, 1);
+            sb.AppendLine();
+            sb.Append(writeFrequencies(t[1].Result, buflen, 2));
+            return sb;
+        });
+
+        Console.WriteLine(taskString1and2.Result);
+        Console.WriteLine(taskString3.Result);
+        Console.WriteLine(taskString4.Result);
+        Console.WriteLine(taskString6.Result);
+        Console.WriteLine(taskString12.Result);
+        Console.WriteLine(taskString18.Result);
     }
-
-    // static void process()
-    // {
-    //     const byte A = (byte)'a';//, C = (byte)'c', G = (byte)'g', T = (byte)'t';
-    //     while(true)
-    //     {
-    //         bool finished = threeEnd>=0;
-    //         int countId=0,working=0,lastId=threeBlockLastId;
-    //         for(; countId<COUNT_LENGTH; countId++)
-    //         {
-    //             working = countBlockWorking[countId];
-    //             if(working!=-1 && working<=lastId
-    //             && Interlocked.CompareExchange(ref countBlockWorking[countId], -1, working)==working) break;
-    //         }
-            
-    //         if(countId==COUNT_LENGTH)
-    //         {
-    //             if(finished) break;
-    //             Thread.Sleep(0);
-    //             continue;
-    //         }
-            
-    //         // work block working
-    //         if(working==0)
-    //         {
-    //             var dictionary = countDictionary[countId] = new Dictionary<long,WrapperImproved>();
-    //             int i = threeStart;
-
-    //             long rollingKey = 0;
-    //             long mask = 0;
-    //             var block = threeBlocks[working];
-    //             var cursorEnd = countKeyLengths[countId]-1;
-    //             for (int cursor = 0; cursor<cursorEnd; cursor++)
-    //             {
-    //                 rollingKey <<= 2;
-    //                 rollingKey |= tonum[block[i]];
-    //                 mask = (mask << 2) + 3;
-                    
-    //                 if(++i==READER_BUFFER_SIZE)
-    //                 {
-    //                     while(threeBlocks[1]==null) Thread.Sleep(0);
-    //                     block = threeBlocks[1];
-    //                     working = 1;
-    //                     i = 0;
-    //                 }
-    //             }
-    //             mask = (mask << 2) + 3;
-    //             countMask[countId] = mask;
-
-    //             for(;i<block.Length;i++)
-    //             {
-    //                 var cursorByte = block[i];
-    //                 if(cursorByte>=A)
-    //                 {
-    //                     rollingKey = ((rollingKey << 2) & mask) | tonum[cursorByte];
-    //                     WrapperImproved w;
-    //                     if (dictionary.TryGetValue(rollingKey, out w))
-    //                         w.v++;
-    //                     else
-    //                         dictionary.Add(rollingKey, new WrapperImproved());
-    //                 }
-    //             }
-    //             countRollingKey[countId] = rollingKey;
-    //         }
-    //         else
-    //         {
-    //             var dictionary = countDictionary[countId];
-    //             var rollingKey = countRollingKey[countId];
-    //             var mask = countMask[countId];
-    //             foreach(var cursorByte in threeBlocks[working])
-    //             {
-    //                 if(cursorByte>=A)
-    //                 {   
-    //                     rollingKey = ((rollingKey << 2) & mask) | tonum[cursorByte];
-    //                     WrapperImproved w;
-    //                     if (dictionary.TryGetValue(rollingKey, out w))
-    //                         w.v++;
-    //                     else
-    //                         dictionary.Add(rollingKey, new WrapperImproved());
-    //                 }
-    //             }
-    //             countRollingKey[countId] = rollingKey;
-    //         }
-
-    //         // end block working
-
-    //         countBlockWorking[countId] = working+1;
-    //     }
-    // }
 }
