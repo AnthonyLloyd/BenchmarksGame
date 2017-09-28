b9b4fd6f47abe908f60152e194d387ccb5dc82df
Anthony Lloyd <anthony@lloydhq.com>
2017-09-28 20:47:19 +0100

    2.3%

--- csharp/MandelbrotImproved.cs
+++ csharp/MandelbrotImproved.cs
@@ -9,7 +9,7 @@
 using System;
 using System.Threading.Tasks;
 
-public class MandelBrot
+public static class MandelBrot
 {
     public static byte[] Main (String[] args)
     {
@@ -20,17 +20,15 @@ public class MandelBrot
         var data = new byte[n*lineLen];
         Parallel.For(0, n, y =>
         {
-            var Ciby = y*invN-1.0;
-            var offset = y*lineLen;
+            var Ciby = y*invN - 1.0;
             for(int x=0; x<lineLen; x++)
             {
                 var Crbx = x*8*invN - 1.5;
                 int res = 0;
                 for(int i=0; i<4; i++)
                 {
-                    double Crbx1=Crbx+invN;
-                    double Zr1=Crbx, Zr2=Crbx1;
-                    double Zi1=Ciby, Zi2=Ciby;
+                    double Zi1=Ciby, Zi2=Ciby, Zr1=Crbx;
+                    double Crbx1=Crbx+invN, Zr2=Crbx1;
 
                     int b=0;
                     int j=49;
@@ -44,13 +42,13 @@ public class MandelBrot
                         Zi2=Zr2*Zi2*2+Ciby;
                         Zr2=nZr2;
 
-                        if(Zr1*Zr1+Zi1*Zi1>4){b|=2;if(b==3)break;}
-                        if(Zr2*Zr2+Zi2*Zi2>4){b|=1;if(b==3)break;}
+                        if(Zr1*Zr1+Zi1*Zi1>4.0){b|=2;if(b==3)break;}
+                        if(Zr2*Zr2+Zi2*Zi2>4.0){b|=1;if(b==3)break;}
                     } while(--j>0);
                     res=(res<<2)+b;
                     Crbx = Crbx1+invN;
                 }
-                data[offset++] = (byte)(res^-1);
+                data[y*lineLen+x] = (byte)~res;
             }
         });
         //Console.OpenStandardOutput().Write(data, 0, data.Length);
