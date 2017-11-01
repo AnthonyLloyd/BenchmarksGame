797b6d3d2dca71eae1766588e6c6c9baf651622b
Anthony Lloyd <anthony@lloydhq.com>
2017-11-01 20:47:27 +0000

    zeroCreate and for loops

--- fsharp/Fasta.fs
+++ fsharp/Fasta.fs
@@ -18,8 +18,7 @@ let inline private cumsum (a:float[]) =
 let createGenerator (vs:byte[]) (ps:float[]) =
     cumsum ps
     let e = ps.Length-1
-    fun () ->
-        let p = rnd()
+    fun p ->
         let rec search i =
             if i=e then e
             elif ps.[i]>=p then i
@@ -31,38 +30,53 @@ let Width = 60
 [<Literal>]
 let Width1 = 61
 [<Literal>]
-let Lines = 8
+let LinesPerBlock = 1024
 
+// type SpinLock() =
+//     let s = System.Threading.SpinLock false
+//     member __.Run f =
+//         let lockTaken = ref false
+//         try
+//             s.Enter lockTaken
+//             f()
+//         finally
+//             if !lockTaken then s.Exit()
 
 [<EntryPoint>]
 let main args =
     let n = if args.Length=0 then 1000 else int args.[0]
-    let out = System.Console.OpenStandardOutput()
+    let out = System.Console.OpenStandardOutput()//System.IO.Stream.Null//
     let inline writeSub (bs:byte[]) l = out.Write(bs,0,l)
     let inline write bs = writeSub bs bs.Length
 
-    let writeRepeat n (table:byte[]) =
-        let tableLength = table.Length
-        let repeatedBytesLength = tableLength*Width1
-        let repeatedBytes =
-            Array.init repeatedBytesLength
-              (fun i -> if i%Width1=0 then '\n'B else table.[(i-1-i/Width1)%tableLength])
-        for __ = 1 to n/(tableLength*Width) do write repeatedBytes
-        let remaining = n%(tableLength*Width)
-        if remaining<>0 then writeSub repeatedBytes (1+remaining+(remaining-1)/Width)
+    let inline writeRepeat n tableLength (table:byte[]) =
+        let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
+        let repeatedBytes = Array.zeroCreate (Width1*linesPerBlock)
+        for i = 0 to linesPerBlock*Width-1 do
+            repeatedBytes.[i+i/Width] <- table.[i%tableLength]
+        for i = 1 to linesPerBlock do
+            repeatedBytes.[i*Width1-1] <- '\n'B
+        for __ = 1 to (n-1)/(Width*linesPerBlock) do write repeatedBytes        
+        let remaining = (n-1)%(Width*linesPerBlock)+1
+        if remaining<>0 then
+            writeSub repeatedBytes (remaining+(remaining-1)/Width)
 
-    let writeRandom n gen =
-        let length = Width1*Lines
-        let bytes l =
-            Array.init l
-              (fun i -> if i%Width1=0 then '\n'B else gen())
-        for __ = 1 to n/(Width*Lines) do bytes length |> write
-        let remaining = n%(Width*Lines)
-        if remaining<>0 then bytes (1+remaining+(remaining-1)/Width) |> write
+    let writeRandom n d gen =
+        let bytes l d =
+            let a = Array.zeroCreate (l+(l+d)/Width)
+            for i = 0 to l-1 do
+                a.[i+i/Width] <- rnd() |> gen
+            for i = 1 to (l+d)/Width do
+                a.[i*Width1-1] <- '\n'B
+            a            
+        for __ = 1 to (n-1)/(Width*LinesPerBlock) do
+            bytes (Width*LinesPerBlock) 0 |> write
+        let remaining = (n-1)%(Width*LinesPerBlock)+1
+        if remaining<>0 then bytes remaining d |> write
 
-    write ">ONE Homo sapiens alu"B
+    write ">ONE Homo sapiens alu\n"B
     
-    writeRepeat (2*n)
+    writeRepeat (2*n) 287
         "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
          GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
          CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
@@ -71,19 +85,17 @@ let main args =
          AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
          AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
 
-    write "\n>TWO IUB ambiguity codes"B
+    write "\n>TWO IUB ambiguity codes\n"B
 
     createGenerator "acgtBDHKMNRSVWY"B
         [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
-    |> writeRandom (3*n)
+    |> writeRandom (3*n) -1
 
-    write "\n>THREE Homo sapiens frequency"B
+    write "\n>THREE Homo sapiens frequency\n"B
 
     createGenerator "acgt"B 
         [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
-    |> writeRandom (5*n)
-
-    out.WriteByte '\n'B
+    |> writeRandom (5*n) 0
     
     0
 
