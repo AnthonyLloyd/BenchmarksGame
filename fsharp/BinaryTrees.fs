ad6b9cb68098828ffef474ca226e7a79d6085072
Anthony Lloyd <anthony@lloydhq.com>
2017-11-08 00:24:32 +0000

    small

--- fsharp/BinaryTrees.fs
+++ fsharp/BinaryTrees.fs
@@ -8,11 +8,7 @@
 // *reset*
 
 type Next = { Left: Tree; Right: Tree }
-and [<Struct>] Tree(n:Next) =
-    member __.Check() =
-        match box n with 
-        | null -> 1
-        | _ -> 1 + n.Left.Check() + n.Right.Check()
+and [<Struct>] Tree = | Tree of Next
 
 [<EntryPoint>]
 let main args =
@@ -24,31 +20,30 @@ let main args =
         if depth=0 then Tree Unchecked.defaultof<_>
         else Tree {Left = make (depth-1); Right = make (depth-1)}
 
+    let rec check (Tree n) = if box n |> isNull then 1
+                             else 1 + check n.Left + check n.Right
+
     let stretchCheck = System.Threading.Tasks.Task.Run(fun () ->
-        let check = (make stretchDepth).Check().ToString()
+        let check = make stretchDepth |> check |> string
         "stretch tree of depth "+string stretchDepth+"\t check: "+check )
 
     let longLivedTree = System.Threading.Tasks.Task.Run(fun () ->
         let tree = make maxDepth
-        let check = tree.Check().ToString()
-        let s = "long lived tree of depth "+string maxDepth+"\t check: "+check
-        tree, s )
+        let check = check tree |> string
+        tree, "long lived tree of depth "+string maxDepth+"\t check: "+check )
     
     let loopDepths = Array.init ((maxDepth-minDepth)/2+1) (fun d ->
         let d = minDepth+d*2
         let n = 1 <<< (maxDepth - d + minDepth)
         let c = ref 0
         System.Threading.Tasks.Parallel.For(0, n, (fun () -> 0),
-            (fun _ _ subtotal -> subtotal + (make d).Check()),
+            (fun _ _ subtotal -> subtotal + (make d |> check)),
             (fun x -> System.Threading.Interlocked.Add(c, x) |> ignore)
         ) |> ignore
-        string n+"\t trees of depth "+string d+"\t check: "+string !c
-    )
+        string n+"\t trees of depth "+string d+"\t check: "+string !c )
 
     stretchCheck.Result |> stdout.WriteLine
-
     loopDepths |> Array.iter stdout.WriteLine
-
     snd longLivedTree.Result |> stdout.WriteLine
     
     0
