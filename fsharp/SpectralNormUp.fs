748c6ab962fd34f58817d46748f8b5200f63fcca
Anthony Lloyd <anthony@lloydhq.com>
2017-12-20 20:23:32 +0000

    small

--- fsharp/SpectralNormUp.fs
+++ fsharp/SpectralNormUp.fs
@@ -9,7 +9,7 @@ module SpectralNormUp
 open System
 open System.Threading
 
-let Approximate(u:double[], v:double[], tmp:double[], rbegin, rend, barrier: Barrier) =
+let approximate u v tmp rbegin rend (barrier: Barrier) =
 
     let mutable vBv = 0.0
     let mutable vv = 0.0
@@ -18,33 +18,33 @@ let Approximate(u:double[], v:double[], tmp:double[], rbegin, rend, barrier: Bar
     let inline A i j = 1.0 / float((i + j) * (i + j + 1) / 2 + i + 1)
     let inline At i j = A j i
     // multiply vector v by matrix A 
-    let inline multiplyAv (v:double[]) (Av:double[]) A =
+    let inline multiplyAv v Av A =
         for i = rbegin to rend - 1 do
             let mutable sum = 0.0
-            for j = 0 to v.Length - 1 do
-                sum <- sum + A i j * v.[j]
-            Av.[i] <- sum
+            for j = 0 to Array.length v - 1 do
+                sum <- sum + A i j * Array.get<float> v j
+            Array.set Av i sum
 
     // multiply vector v by matrix A and then by matrix A transposed
-    let inline multiplyatAv(v:double[], tmp:double[], atAv:double[]) =
+    let inline multiplyatAv v tmp atAv =
         multiplyAv v tmp A
         barrier.SignalAndWait()
         multiplyAv tmp atAv At
         barrier.SignalAndWait()
 
     for __ = 0 to 9 do
-        multiplyatAv(u, tmp, v)
-        multiplyatAv(v, tmp, u)
+        multiplyatAv u tmp v
+        multiplyatAv v tmp u
 
     for i = rbegin to rend - 1 do
         let vi = Array.get v i
         vv <- vv + vi * vi
         vBv <- vBv + vi * Array.get u i
 
-    (vBv, vv)
+    vBv, vv
 
 
-let inline RunGame n =
+let runGame n =
     // create unit vector
     let u = Array.create n 1.0
     let tmp = Array.zeroCreate n
@@ -59,9 +59,9 @@ let inline RunGame n =
     let aps = 
         Async.Parallel 
           [ for i in 0 .. nthread - 1 do
-                let r1 = i * chunk;
+                let r1 = i * chunk
                 let r2 = if (i < (nthread - 1)) then r1 + chunk else n
-                yield async { return Approximate(u, v, tmp, r1, r2, barrier) } ]
+                yield async { return approximate u v tmp r1 r2 barrier } ]
          |> Async.RunSynchronously
 
     let vBv = aps |> Array.sumBy fst
@@ -73,6 +73,6 @@ let inline RunGame n =
 let main (args:string[]) =
     let n = try int <| args.[0] with _ -> 2500
 
-    RunGame n
+    runGame n
     //System.Console.WriteLine("{0:f9}", RunGame n);
     //0
