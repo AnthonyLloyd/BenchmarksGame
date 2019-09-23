fd4937bca96aaa09259344f61957c0a103c0467b
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-09-23 11:26:21 +0100

    back to basics with a better idea

--- fsharp/FastaNew.fs
+++ fsharp/FastaNew.fs
@@ -21,7 +21,7 @@ open System.Threading.Tasks
 let main (args:string []) =
     let n = if args.Length=0 then 1000 else Int32.Parse(args.[0])
     let out = new IO.MemoryStream()//Console.OpenStandardOutput()
-    let tasks = Array.zeroCreate ((3*n-1)/BlockSize+(5*n-1)/BlockSize+4)
+    let tasks = Array.zeroCreate ((3*n-1)/BlockSize+(5*n-1)/BlockSize+3)
     let bytePool = Buffers.ArrayPool.Shared
     
     let writeRandom n offset d seed (vs:byte[]) (ps:float[]) =
@@ -52,60 +52,72 @@ let main (args:string []) =
             for i = 1 to (l+d)/Width do
                 a.[i*Width1-1] <- '\n'B
             a, l+(l+d)/Width
-        for i = offset+1 to offset+(n-1)/BlockSize do
+        for i = offset to offset+(n-1)/BlockSize-1 do
             let rnds = rnds BlockSize
             tasks.[i] <- Task.Run(fun () -> bytes BlockSize 0 rnds)
         let remaining = (n-1)%BlockSize+1
         let rnds = rnds remaining
-        tasks.[offset+(n-1)/BlockSize+1] <-
+        tasks.[offset+(n-1)/BlockSize] <-
             Task.Run(fun () -> bytes remaining d rnds)
         seed
 
-    tasks.[0] <- Task.Run(fun () ->
-        ">ONE Homo sapiens alu\n"B |> fun i -> out.Write(i,0,i.Length)
-        let table =
-            "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
-             GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
-             CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
-             ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA\
-             GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
-             AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
-             AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
-        let tableLength = 287
-        let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
-        let repeatedBytes = bytePool.Rent (Width1*linesPerBlock)
-        for i = 0 to linesPerBlock*Width-1 do
-            repeatedBytes.[i+i/Width] <- table.[i%tableLength]
-        for i = 1 to linesPerBlock do
-            repeatedBytes.[i*Width1-1] <- '\n'B
-        for __ = 1 to (2*n-1)/(Width*linesPerBlock) do
-            out.Write(repeatedBytes, 0, Width1*linesPerBlock)
-        let remaining = (2*n-1)%(Width*linesPerBlock)+1
-        if remaining<>0 then
-            out.Write(repeatedBytes, 0, remaining+(remaining-1)/Width)
-        bytePool.Return repeatedBytes
-        "\n>TWO IUB ambiguity codes\n"B, 26
-    )
+    Task.Run(fun () ->
+        let seed =
+            writeRandom (3*n) 0 -1 42 "acgtBDHKMNRSVWY"B
+                [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;
+                  0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
+        
+        tasks.[(3*n-1)/BlockSize+1] <-
+            Task.FromResult("\n>THREE Homo sapiens frequency\n"B, 31)
+        
+        writeRandom (5*n) ((3*n-1)/BlockSize+2) 0 seed "acgt"B
+            [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
+        |> ignore
+    ) |> ignore
 
-    let seed =
-        [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;
-            0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
-        |> writeRandom (3*n) 0 -1 42 "acgtBDHKMNRSVWY"B
-         
-    tasks.[(3*n-1)/BlockSize+2] <-
-        Task.FromResult("\n>THREE Homo sapiens frequency\n"B, 31)
+    ">ONE Homo sapiens alu\n"B |> fun i -> out.Write(i,0,i.Length)
+    let table =
+        "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
+         GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
+         CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
+         ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA\
+         GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
+         AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
+         AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
+    let tableLength = 287
+    let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
+    let repeatedBytes = bytePool.Rent (Width1*linesPerBlock)
+    for i = 0 to linesPerBlock*Width-1 do
+        repeatedBytes.[i+i/Width] <- table.[i%tableLength]
+    for i = 1 to linesPerBlock do
+        repeatedBytes.[i*Width1-1] <- '\n'B
+    for __ = 1 to (2*n-1)/(Width*linesPerBlock) do
+        out.Write(repeatedBytes, 0, Width1*linesPerBlock)
+    let remaining = (2*n-1)%(Width*linesPerBlock)+1
+    if remaining<>0 then
+        out.Write(repeatedBytes, 0, remaining+(remaining-1)/Width)
+    bytePool.Return repeatedBytes
+    out.Write("\n>TWO IUB ambiguity codes\n"B, 0, 26)
 
-    [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
-    |> writeRandom (5*n) ((3*n-1)/BlockSize+2) 0 seed "acgt"B
-    |> ignore
-
-    async {
-        for i = 0 to tasks.Length-1 do
+    //async {
+    //    for i = 0 to tasks.Length-1 do
+    //        let t = tasks.[i]
+    //        let! bs,l = Async.AwaitTask t
+    //        out.Write(bs,0,l)
+    //        if l>200 then bytePool.Return bs
+    //    out.WriteByte '\n'B
+    //    return out.ToArray()
+    //}
+    //|> Async.RunSynchronously
+    let rec write i =
+        if i<>tasks.Length then
             let t = tasks.[i]
-            let! bs,l = Async.AwaitTask t
-            out.Write(bs,0,l)
-            if l>200 then bytePool.Return bs
-        out.WriteByte '\n'B
-        return out.ToArray()
-    }
-    |> Async.RunSynchronously
+            (if isNull t then i
+             else
+                let bs,l = t.Result
+                out.Write(bs,0,l)
+                if l>200 then bytePool.Return bs
+                i+1) |> write
+    write 0
+    out.WriteByte '\n'B
+    out.ToArray()
