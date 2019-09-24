67e248a69d2ed6e36cca77669ec4c1b94035d62a
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-09-24 21:50:18 +0100

    io working 150% slower

--- fsharp/FastaNew.fs
+++ fsharp/FastaNew.fs
@@ -12,25 +12,63 @@ let Width1 = 61
 [<Literal>]
 let LinesPerBlock = 2048
 
+[<Struct;NoEquality;NoComparison>]
+type IO<'a> = IO of (('a -> unit) -> unit)
+
+module IO =
+    open System.Threading
+    let run (IO run) : unit =
+        use mre = new ManualResetEvent false
+        ThreadPool.UnsafeQueueUserWorkItem (fun _ ->
+            run (fun () -> mre.Set() |> ignore)
+        ,null) |> ignore
+        mre.WaitOne() |> ignore
+    let fork (IO run) : IO<IO<'a>> =
+        IO (fun contFork ->
+            let mutable o = null
+            ThreadPool.UnsafeQueueUserWorkItem (fun _ ->
+                run (fun a ->
+                    let o = Interlocked.CompareExchange(&o, a, null)
+                    if isNull o |> not then (o :?> 'a->unit) a
+                )
+            ,null) |> ignore
+            IO (fun cont ->
+                let o = Interlocked.CompareExchange(&o, cont, null)
+                if isNull o |> not then cont (o :?> 'a)
+            ) |> contFork
+        )
+
+type IOBuilder() =
+    member inline _.Bind(IO run, f:'a->IO<'b>) : IO<'b> =
+        IO (fun cont ->
+            run (fun o ->
+                let (IO run) = f o
+                run cont
+            )
+        )
+    member inline _.Return value = IO (fun cont -> cont value)
+    member inline _.Zero() = IO (fun cont -> cont Unchecked.defaultof<_>)
+
+let io = IOBuilder()
+
 open System
-open System.Threading.Tasks
 
 //[<EntryPoint>]
 let main (args:string []) =
     let n = if args.Length=0 then 1000 else Int32.Parse(args.[0])
     let out = new IO.MemoryStream()//Console.OpenStandardOutput()
-    let tasks = 
-        ((3*n-1)/(Width*LinesPerBlock)+(5*n-1)/(Width*LinesPerBlock)+3)
-        |> Array.zeroCreate 
+    let noTasks = (3*n-1)/(Width*LinesPerBlock)+(5*n-1)/(Width*LinesPerBlock)+3
+    let randsIO = Array.zeroCreate noTasks
+    let bytesIO = Array.zeroCreate noTasks
     let bytePool = Buffers.ArrayPool.Shared
     let intPool = Buffers.ArrayPool.Shared
 
-    let writeRandom n offset d seed (vs:byte[]) (ps:float[]) =
-        // cumulative probability
-        let mutable total = ps.[0]
-        for i = 1 to ps.Length-1 do
-            total <- total + ps.[i]
-            ps.[i] <- total
+    let rec writeRandom nn offset d seed (vs:byte[]) (ps:float[]) = io {
+        do // cumulative probability
+            let mutable total = ps.[0]
+            for i = 1 to ps.Length-1 do
+                total <- total + ps.[i]
+                ps.[i] <- total
         
         let mutable seed = seed
         let inline rnds l =
@@ -39,7 +77,7 @@ let main (args:string []) =
                 seed <- (seed * 3877 + 29573) % 139968
                 a.[i] <- seed
             a
-        let bytes l d (rnds:int[]) =
+        let inline bytes l d (rnds:int[]) =
             let a = bytePool.Rent (l+(l+d)/Width)
             let inline lookup probability =
                 let rec search i =
@@ -53,70 +91,93 @@ let main (args:string []) =
                 a.[i*Width1-1] <- '\n'B
             a, l+(l+d)/Width
 
-        for i = offset to offset+(n-1)/(Width*LinesPerBlock)-1 do
-            let rnds = rnds (Width*LinesPerBlock)
-            tasks.[i] <- Task.Run(fun () -> bytes (Width*LinesPerBlock) 0 rnds)
+        let lastFullBlock = offset+(nn-1)/(Width*LinesPerBlock)-1
+        let rec createBlock i = io {
+            let rndsA = rnds (Width*LinesPerBlock)
+            let! a = io { return bytes (Width*LinesPerBlock) 0 rndsA } |> IO.fork
+            bytesIO.[i] <- a
+            if i = lastFullBlock then
+                let! r = io {
+                    let remaining = (nn-1)%(Width*LinesPerBlock)+1
+                    let rnds = rnds remaining
+                    let! a = io { return bytes remaining d rnds } |> IO.fork
+                    bytesIO.[i+1] <- a
+                    if offset=0 then
+                        let! next = 
+                            writeRandom (5*n) ((3*n-1)/(Width*LinesPerBlock)+2) 0 seed "acgt"B
+                                [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
+                            |> IO.fork                    
+                        randsIO.[i+3] <- next
+                        return 1
+                    else
+                        return 1 } |> IO.fork
+                randsIO.[i+1] <- r
+                return 1
+            else
+                let! r = createBlock (i+1) |> IO.fork
+                randsIO.[i+1] <- r
+                return 1
+        }
 
-        //let nBlocks = offset+(n-1)/(Width*LinesPerBlock)-1
-        //let rec createBlock i =
-        //    let rnds = rnds (Width*LinesPerBlock)
-        //    tasks.[i] <- Task.Run(fun () -> bytes (Width*LinesPerBlock) 0 rnds)
-        //    if i < nBlocks then createBlock (i+1)
-        //createBlock offset
+        let! rr = createBlock offset
 
-        let remaining = (n-1)%(Width*LinesPerBlock)+1
-        let rnds = rnds remaining
-        tasks.[offset+(n-1)/(Width*LinesPerBlock)] <-
-            Task.Run(fun () -> bytes remaining d rnds)
-        seed
+        return rr
+    }
 
-    Task.Run(fun () ->
-        let seed =
+    let inline first() =
+        ">ONE Homo sapiens alu\n"B |> fun i -> out.Write(i,0,i.Length)
+        let table =
+            "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
+             GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
+             CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
+             ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA\
+             GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
+             AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
+             AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
+        let tableLength = 287
+        let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
+        let repeatedBytes = bytePool.Rent (Width1*linesPerBlock)
+        for i = 0 to linesPerBlock*Width-1 do
+            repeatedBytes.[i+i/Width] <- table.[i%tableLength]
+        for i = 1 to linesPerBlock do
+            repeatedBytes.[i*Width1-1] <- '\n'B
+        for _ = 1 to (2*n-1)/(Width*linesPerBlock) do
+            out.Write(repeatedBytes, 0, Width1*linesPerBlock)
+        let remaining = (2*n-1)%(Width*linesPerBlock)+1
+        if remaining<>0 then
+            out.Write(repeatedBytes, 0, remaining+(remaining-1)/Width)
+        bytePool.Return repeatedBytes
+        out.Write("\n>TWO IUB ambiguity codes\n"B, 0, 26)
+
+    io {
+
+        let! r =
             writeRandom (3*n) 0 -1 42 "acgtBDHKMNRSVWY"B
                 [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;
-                  0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
-        
-        tasks.[(3*n-1)/(Width*LinesPerBlock)+1] <-
-            Task.FromResult("\n>THREE Homo sapiens frequency\n"B, 31)
-        
-        writeRandom (5*n) ((3*n-1)/(Width*LinesPerBlock)+2) 0 seed "acgt"B
-            [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
-        |> ignore
-    ) |> ignore
+                    0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
+            |> IO.fork
+        randsIO.[0] <- r
 
-    ">ONE Homo sapiens alu\n"B |> fun i -> out.Write(i,0,i.Length)
-    let table =
-        "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
-         GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
-         CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
-         ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA\
-         GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
-         AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
-         AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
-    let tableLength = 287
-    let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
-    let repeatedBytes = bytePool.Rent (Width1*linesPerBlock)
-    for i = 0 to linesPerBlock*Width-1 do
-        repeatedBytes.[i+i/Width] <- table.[i%tableLength]
-    for i = 1 to linesPerBlock do
-        repeatedBytes.[i*Width1-1] <- '\n'B
-    for __ = 1 to (2*n-1)/(Width*linesPerBlock) do
-        out.Write(repeatedBytes, 0, Width1*linesPerBlock)
-    let remaining = (2*n-1)%(Width*linesPerBlock)+1
-    if remaining<>0 then
-        out.Write(repeatedBytes, 0, remaining+(remaining-1)/Width)
-    bytePool.Return repeatedBytes
-    out.Write("\n>TWO IUB ambiguity codes\n"B, 0, 26)
+        first()
 
-    let rec write i =
-        if i<>tasks.Length then
-            let t = tasks.[i]
-            (if isNull t then i
-             else
-                let bs,l = t.Result
+        randsIO.[(3*n-1)/(Width*LinesPerBlock)+1] <-
+            io { return 1 }
+        bytesIO.[(3*n-1)/(Width*LinesPerBlock)+1] <-
+            io { return "\n>THREE Homo sapiens frequency\n"B, 31 }
+
+        let rec write i = io {
+            if i < noTasks then
+                let! _ =  randsIO.[i]
+                let! bs,l = bytesIO.[i]
                 out.Write(bs,0,l)
-                if l>200 then bytePool.Return bs
-                i+1) |> write
-    write 0
-    out.WriteByte '\n'B
+                do if l>200 then bytePool.Return bs
+                do! write (i+1)
+        }
+
+        do! write 0
+
+        out.WriteByte '\n'B
+
+    } |> IO.run
+
     out.ToArray()
--- tester/Program.fs
+++ tester/Program.fs
@@ -1,6 +1,17 @@
 ﻿open System
 open Expecto
 
+let private allDiffs (s1:byte array) (s2:byte array) =
+  Seq.mapi2 (fun i s p -> i,s,p) s1 s2
+  |> Seq.take (min s1.Length s2.Length)
+  |> Seq.fold (fun (onFrom,l) v ->
+      match onFrom, v with
+      | None,(i,f,s) when f<>s -> Some i, l
+      | Some i,(j,f,s) when f=s -> None, (i,j)::l
+      | onFrom,_ -> onFrom, l
+  ) (None,[])
+  |> snd
+
 /// Expects function `f1` is faster than `f2`. Measurer used to measure only a
 /// subset of the functions. Statistical test to 99.99% confidence level.
 let isFasterThanSub (f1:Performance.Measurer<_,_>->'a) (f2:Performance.Measurer<_,_>->'a) format =
@@ -10,6 +21,12 @@ let isFasterThanSub (f1:Performance.Measurer<_,_>->'a) (f2:Performance.Measurer<
   match Performance.timeCompare f1 f2 with
   | Performance.ResultNotTheSame (r1, r2)->
     printfn "%s. Expected function results to be the same (%A vs %A)." format r1 r2
+    match box r1, box r2 with
+    | (:? (byte[]) as r1), (:? (byte[]) as r2) ->
+        printfn "r1 length: %i" r1.Length
+        printfn "r2 length: %i" r2.Length
+        printfn "all diffs:\n%A" (allDiffs r1 r2)
+    | _ -> ()
   | Performance.MetricTooShort (s,p) ->
     printfn "%s. Expected metric (%s) to be much longer than the machine resolution (%s)." format (toString s) (toString p)
   | Performance.MetricEqual (s1,s2) ->
