12f135e42ca16d1a6d5fc96c1fa01dd338c9c1e9
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-09-27 18:03:00 +0100

    still good

--- fsharp/FastaNew.fs
+++ fsharp/FastaNew.fs
@@ -18,8 +18,8 @@ open System
 let main (args:string []) =
     let n = if args.Length=0 then 1000 else Int32.Parse(args.[0])
     let out = new IO.MemoryStream()//Console.OpenStandardOutput()
-    let tasks = (3*n-1)/(Width*LinesPerBlock)+(5*n-1)/(Width*LinesPerBlock)+3
-                |> Array.zeroCreate
+    let blocks = (3*n-1)/(Width*LinesPerBlock)+(5*n-1)/(Width*LinesPerBlock)+3
+                 |> Array.zeroCreate
     let bytePool = Buffers.ArrayPool.Shared
     let intPool = Buffers.ArrayPool.Shared
     
@@ -57,7 +57,7 @@ let main (args:string []) =
                 let rnds = rnds (Width*LinesPerBlock) i
                 Threading.ThreadPool.QueueUserWorkItem(fun o ->
                     let rnds = o :?> int[]
-                    tasks.[rnds.[Width*LinesPerBlock]] <-
+                    blocks.[rnds.[Width*LinesPerBlock]] <-
                         box(bytes (Width*LinesPerBlock) rnds, Width1*LinesPerBlock)
                 , rnds) |> ignore
                 
@@ -65,18 +65,17 @@ let main (args:string []) =
             let rnds = rnds remaining (offset+(n-1)/(Width*LinesPerBlock))
             Threading.ThreadPool.QueueUserWorkItem(fun o ->
                 let rnds = o :?> int[]
-                tasks.[rnds.[remaining]] <-
+                blocks.[rnds.[remaining]] <-
                     box(bytes remaining rnds, remaining+(remaining-1)/Width+1)
             , rnds) |> ignore
             seed 
            
-        let seed =
-            [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;
-              0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
-            |> writeRandom (3*n) 0 42 "acgtBDHKMNRSVWY"B
+        let seed = writeRandom (3*n) 0 42 "acgtBDHKMNRSVWY"B
+                    [|0.27;0.12;0.12;0.27;0.02;0.02;0.02;
+                      0.02;0.02;0.02;0.02;0.02;0.02;0.02;0.02|]
 
-        [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|]
-        |> writeRandom (5*n) ((3*n-1)/(Width*LinesPerBlock)+2) seed "acgt"B
+        writeRandom (5*n) ((3*n-1)/(Width*LinesPerBlock)+2) seed "acgt"B
+            [|0.3029549426680;0.1979883004921;0.1975473066391;0.3015094502008|] 
         |> ignore
     , null) |> ignore
 
@@ -89,11 +88,10 @@ let main (args:string []) =
          GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
          AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
          AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"B
-    let tableLength = 287
-    let linesPerBlock = (LinesPerBlock/tableLength+1) * tableLength
+    let linesPerBlock = (LinesPerBlock/287+1) * 287
     let repeatedBytes = bytePool.Rent (Width1*linesPerBlock)
     for i = 0 to linesPerBlock*Width-1 do
-        repeatedBytes.[1+i+i/Width] <- table.[i%tableLength]
+        repeatedBytes.[1+i+i/Width] <- table.[i%287]
     for i = 0 to (Width*linesPerBlock-1)/Width do
         repeatedBytes.[i*Width1] <- '\n'B
     for __ = 1 to (2*n-1)/(Width*linesPerBlock) do
@@ -103,14 +101,14 @@ let main (args:string []) =
     bytePool.Return repeatedBytes
     out.Write("\n>TWO IUB ambiguity codes"B,0,25)
 
-    tasks.[(3*n-1)/(Width*LinesPerBlock)+1] <-
+    blocks.[(3*n-1)/(Width*LinesPerBlock)+1] <-
         box("\n>THREE Homo sapiens frequency"B, 30)
 
-    for i = 0 to tasks.Length-1 do
-        let mutable t = tasks.[i]
+    for i = 0 to blocks.Length-1 do
+        let mutable t = blocks.[i]
         while isNull t do
             Threading.Thread.Sleep 0
-            t <- tasks.[i]
+            t <- blocks.[i]
         let bs,l = t :?> byte[]*int
         out.Write(bs,0,l)
         if l>200 then bytePool.Return bs
