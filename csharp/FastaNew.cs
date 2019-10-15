e0fa03a5f1e2e6eedc42a0d9dad69487f7e2911c
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-10-15 22:50:21 +0100

    faste.cs 16%

--- csharp/FastaNew.cs
+++ csharp/FastaNew.cs
@@ -18,6 +18,7 @@ public/**/ class FastaNew
     const int Width1 = 61;
     const int LinesPerBlock = 2048;
     const int BlockSize = Width * LinesPerBlock;
+    const int BlockSize1 = Width1 * LinesPerBlock;
     const int IM = 139968;
     const int IA = 3877;
     const int IC = 29573;
@@ -26,52 +27,56 @@ public/**/ class FastaNew
     static readonly ArrayPool<int> intPool = ArrayPool<int>.Shared;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    static byte[] Bytes(int i, int[] rnds, byte[] vs, double[] ps)
+    static byte[] Bytes(int i, int[] rnds, byte[] vs, int[] ps)
     {
-        int r = 0, w = 0;
-        var a = bytePool.Rent(Width1 * LinesPerBlock);
-        var s = a.AsSpan(0, (i-1)/Width + i + 1);
-        for (i = 0; i < s.Length; i++)
+        var a = bytePool.Rent(BlockSize1);
+        var s = a.AsSpan(0, (i - 1) / Width + i + 1);
+        for (i = 1; i < s.Length; i++)
         {
-            if (w-- == 0)
+            var probability = rnds[i];
+            int j = 0;
+            while (ps[j] < probability) j++;
+            s[i] = vs[j];
+        }
+        return a;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static int[] Rnds(int i, int j,  ref int seed)
+    {
+        var a = intPool.Rent(BlockSize1);
+        var s = a.AsSpan(0, (i - 1) / Width + i + 1);
+        s[0] = j;
+        for (i = 1, j = Width; i < s.Length; i++)
+        {
+            if (j-- == 0)
             {
-                w = Width;
-                s[i] = (byte)'\n';
+                j = Width;
+                s[i] = IM * 3 / 2;
             }
             else
             {
-                var probability = rnds[r++];
-                int j = 0;
-                while (ps[j] < probability) j++;
-                s[i] = vs[j];
+                s[i] = seed = (seed * IA + IC) % IM;
             }
         }
         return a;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    static int[] Rnds(int l, int j,  ref int seed)
+    static int WriteRandom(int n, int offset, int seed, byte[] vs, double[] ps,
+        Tuple<byte[], int>[] blocks)
     {
-        var a = intPool.Rent(BlockSize + 1);
-        var s = a.AsSpan(0, l);
-        for (int i = 0; i < s.Length; i++)
-            s[i] = seed = (seed * IA + IC) % IM;
-        a[l] = j;
-        return a;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    static int WriteRandom(int n, int offset, int seed, byte[] vs, double[] ps, Tuple<byte[], int>[] blocks)
-    {
-        var total = 0.0; // cumulative probability
-        for (int i = 0; i < ps.Length; i++) ps[i] = total += ps[i] * IM;
+        // make cumulative int
+        var psIM = new int[ps.Length];
+        var total = 0.0;
+        for (int i = 0; i < ps.Length; i++)
+            psIM[i] = (int)(total += ps[i] * IM);
 
         void create(object o)
         {
             var rnds = (int[])o;
-            blocks[rnds[BlockSize]] =
-            Tuple.Create(Bytes(BlockSize, rnds, vs, ps),
-                Width1 * LinesPerBlock);
+            blocks[rnds[0]] =
+            Tuple.Create(Bytes(BlockSize, rnds, vs, psIM), BlockSize1);
             intPool.Return(rnds);
         }
 
@@ -79,15 +84,16 @@ public/**/ class FastaNew
 
         for (int i = offset; i < offset + (n - 1) / BlockSize; i++)
         {
-            ThreadPool.QueueUserWorkItem(createDel, Rnds(BlockSize, i, ref seed));
+            ThreadPool.QueueUserWorkItem(createDel,
+                Rnds(BlockSize, i, ref seed));
         }
 
         var remaining = (n - 1) % BlockSize + 1;
         ThreadPool.QueueUserWorkItem(o =>
         {
             var rnds = (int[])o;
-            blocks[rnds[remaining]] =
-            Tuple.Create(Bytes(remaining, rnds, vs, ps),
+            blocks[rnds[0]] =
+            Tuple.Create(Bytes(remaining, rnds, vs, psIM),
                 remaining + (remaining - 1) / Width + 1);
             intPool.Return(rnds);
         }, Rnds(remaining, offset + (n - 1) / BlockSize, ref seed));
@@ -99,7 +105,8 @@ public/**/ class FastaNew
     {
         int n = args.Length == 0 ? 1000 : int.Parse(args[0]);
         var o = new System.IO.MemoryStream();//Console.OpenStandardOutput();
-        var blocks = new Tuple<byte[], int>[(3 * n - 1) / BlockSize + (5 * n - 1) / BlockSize + 3];
+        var blocks = new Tuple<byte[], int>[
+            (3 * n - 1) / BlockSize + (5 * n - 1) / BlockSize + 3];
 
         ThreadPool.QueueUserWorkItem(_ =>
         {
@@ -107,15 +114,16 @@ public/**/ class FastaNew
                 new[] { (byte)'a', (byte)'c', (byte)'g', (byte)'t',
                     (byte)'B', (byte)'D', (byte)'H', (byte)'K', (byte)'M',
                     (byte)'N', (byte)'R', (byte)'S', (byte)'V', (byte)'W',
-                    (byte)'Y'},
-                        new[] {0.27,0.12,0.12,0.27,0.02,0.02,0.02,
-                               0.02,0.02,0.02,0.02,0.02,0.02,0.02,0.02}, blocks);
+                    (byte)'Y', (byte)'\n' },
+                new[] { 0.27,0.12,0.12,0.27,0.02,0.02,0.02,0.02,
+                        0.02,0.02,0.02,0.02,0.02,0.02,0.02,1.00 }, blocks);
 
             WriteRandom(5 * n, (3 * n - 1) / BlockSize + 2, seed,
-                new byte[] { (byte)'a', (byte)'c', (byte)'g', (byte)'t' },
+                new byte[] { (byte)'a', (byte)'c', (byte)'g', (byte)'t',
+                    (byte)'\n' },
                 new[] { 0.3029549426680, 0.1979883004921,
-                        0.1975473066391, 0.3015094502008 }, blocks);
-
+                        0.1975473066391, 0.3015094502008,
+                        1.0 }, blocks);
         });
 
         o.Write(Encoding.ASCII.GetBytes(">ONE Homo sapiens alu"), 0, 21);
@@ -147,6 +155,7 @@ public/**/ class FastaNew
         {
             Tuple<byte[], int> t;
             while ((t = blocks[i]) == null) Thread.Sleep(0);
+            t.Item1[0] = (byte)'\n';
             o.Write(t.Item1, 0, t.Item2);
             if (t.Item2 == Width1 * LinesPerBlock) bytePool.Return(t.Item1);
         }
