819f625c054fa9502abce8c78bfca6bc60a23e53
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-10-29 19:14:52 +0000

    32% yay

--- fsharp/PiDigitNew.fs
+++ fsharp/PiDigitNew.fs
@@ -16,114 +16,76 @@ type MPZ =
     val _size: int
     val ptr: System.IntPtr
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_init",ExactSpelling=true,SetLastError=false)>]
-extern void private mpzInit(MPZ& _value)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_init",ExactSpelling=true)>]
+extern void private mpz_init(MPZ& _value)
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_mul_si",ExactSpelling=true,SetLastError=false)>]
-extern void private mpzMul(MPZ& _dest, MPZ& _src, int _value)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_init_set_ui",ExactSpelling=true)>]
+extern void private mpz_init_set_ui(MPZ& _dest, uint32 _value)
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_add",ExactSpelling=true,SetLastError=false)>]
-extern void private mpzAdd(MPZ& _dest, MPZ& _src, MPZ& _src2)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_mul_ui",ExactSpelling=true)>]
+extern void private mpz_mul_ui(MPZ& _dest, MPZ& _src, uint32 _value)
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_tdiv_q",ExactSpelling=true,SetLastError=false)>]
-extern void private mpzTdiv(MPZ& _dest, MPZ& _src, MPZ& _src2)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_add",ExactSpelling=true)>]
+extern void private mpz_add(MPZ& _dest, MPZ& _src, MPZ& _src2)
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_set_si",ExactSpelling=true,SetLastError=false)>]
-extern void private mpzSet(MPZ& _src, int _value)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_tdiv_q",ExactSpelling=true)>]
+extern void private mpz_tdiv_q(MPZ& _dest, MPZ& _src, MPZ& _src2)
 
-[<DllImport("gmp.so.10",EntryPoint="__gmpz_get_si",ExactSpelling=true,SetLastError=false)>]
-extern int private mpzGet(MPZ& _src)
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_get_ui",ExactSpelling=true)>]
+extern uint32 private mpz_get_ui(MPZ& _src)
+
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_submul_ui",ExactSpelling=true)>]
+extern void private mpz_submul_ui(MPZ& _dest, MPZ& _src, uint32 _value)
+
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_addmul_ui",ExactSpelling=true)>]
+extern void private mpz_addmul_ui(MPZ& _dest, MPZ& _src, uint32 _value)
+
+[<DllImport("gmp.so.10",EntryPoint="__gmpz_cmp",ExactSpelling=true)>]
+extern int private mpz_cmp(MPZ& _op1, MPZ& _op2)
 
 //[<EntryPoint>]
 let main (args:string[]) =
 
-    let init() =
-        let mutable result = MPZ()
-        mpzInit(&result)
-        result
+    let mutable tmp1, tmp2, acc, den, num = MPZ(), MPZ(), MPZ(), MPZ(), MPZ()
+    
+    mpz_init(&tmp1)
+    mpz_init(&tmp2)
+    mpz_init_set_ui(&acc, 0u)
+    mpz_init_set_ui(&den, 1u)
+    mpz_init_set_ui(&num, 1u)
+    
+    let extract_digit nth =
+        mpz_mul_ui(&tmp1, &num, nth)
+        mpz_add(&tmp2, &tmp1, &acc)
+        mpz_tdiv_q(&tmp1, &tmp2, &den)
+        mpz_get_ui(&tmp1)
 
-    let mutable q = init()
-    let mutable r = init()
-    let mutable s = init()
-    let mutable t = init()
-    let mutable u = init()
-    let mutable v = init()
-    let mutable w = init()
+    let eliminate_digit d =
+        mpz_submul_ui(&acc, &den, d)
+        mpz_mul_ui(&acc, &acc, 10u)
+        mpz_mul_ui(&num, &num, 10u)
 
-    mpzSet(&q, 1)
-    mpzSet(&r, 0)
-    mpzSet(&s, 0)
-    mpzSet(&t, 1)
+    let next_term k =
+        let k2 = k * 2u + 1u
+        mpz_addmul_ui(&acc, &num, 2u)
+        mpz_mul_ui(&acc, &acc, k2)
+        mpz_mul_ui(&den, &den, k2)
+        mpz_mul_ui(&num, &num, k)
 
-    let composeR bq br bs bt =
-        let pr = PerfSimple.regionStart "composeR"
-        mpzMul(&u, &r, bs)
-        mpzMul(&r, &r, bq)
-        mpzMul(&v, &t, br)
-        mpzAdd(&r, &r, &v)
-        mpzMul(&t, &t, bt)
-        mpzAdd(&t, &t, &u)
-        mpzMul(&s, &s, bt)
-        mpzMul(&u, &q, bs)
-        mpzAdd(&s, &s, &u)
-        mpzMul(&q, &q, bq)
-        PerfSimple.regionEnd pr
-
-    let composeL bq br bs bt =
-        let pr = PerfSimple.regionStart "composeL"
-        mpzMul(&r, &r, bt)
-        mpzMul(&u, &q, br)
-        mpzAdd(&r, &r, &u)
-        mpzMul(&u, &t, bs)
-        mpzMul(&t, &t, bt)
-        mpzMul(&v, &s, br)
-        mpzAdd(&t, &t, &v)
-        mpzMul(&s, &s, bq)
-        mpzAdd(&s, &s, &u)
-        mpzMul(&q, &q, bq)
-        PerfSimple.regionEnd pr
-
-    let inline extract j =
-        let pr = PerfSimple.regionStart "extract"
-        mpzMul(&u, &q, j)
-        mpzAdd(&u, &u, &r)
-        mpzMul(&v, &s, j)
-        mpzAdd(&v, &v, &t)
-        mpzTdiv(&w, &u, &v)
-        let r = mpzGet(&w)
-        PerfSimple.regionEnd pr
-        r
-
+    let n = uint32 args.[0]
     let out = new System.IO.MemoryStream()//System.Console.OpenStandardOutput()
-    let n = int args.[0]
-    let bytes = Array.zeroCreate 18
-    bytes.[10] <- '\t'B; bytes.[11] <- ':'B
-    bytes.[14] <- '\n'B; bytes.[15] <- '\n'B
-    bytes.[16] <- '\n'B; bytes.[17] <- '\n'B
     let encoding = System.Text.Encoding.ASCII
-    let inline write i =
-        let s = i.ToString()
-        encoding.GetBytes(s, 0, s.Length, bytes, 12) |> ignore
-        out.Write(bytes, 0, s.Length+13)
-    let mutable i,j,k = 0,0,1
-    while i<n do
-        let y = extract 3
-        if y = extract 4 then
-            composeR 10 (y* -10) 0 1
-            bytes.[j] <- byte(y+48)
-            i<-i+1
-            j<-j+1
-            if j=10 then
-                let writePR = PerfSimple.regionStart "write"
-                j<-0
-                write i
-                PerfSimple.regionEnd writePR
-        else
-            composeL k (k*4+2) 0 (k*2+1)
-            k<-k+1
-    if j<>0 then
-        let pr = PerfSimple.regionStart "no"
-        for c = j to 9 do bytes.[c] <- ' 'B
-        write n
-        PerfSimple.regionEnd pr
+    let mutable i, k = 0u, 0u
+    while i < n do
+        k <- k + 1u
+        next_term k
+        if mpz_cmp(&num, &acc) <= 0 then
+            let d = extract_digit 3u
+            if d = extract_digit 4u then
+                out.WriteByte(byte(d+48u))
+                i <- i + 1u
+                if i % 10u = 0u then
+                    let bs = encoding.GetBytes("\t:" + string i + "\n")
+                    out.Write(bs, 0, bs.Length)
+                eliminate_digit d
     out.ToArray()
